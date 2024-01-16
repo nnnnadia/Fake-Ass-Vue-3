9d39afa6b8ea5120ede18f4d611f4d80ad66d8af
nnnnadia <tristaonadia@gmail.com>
2024-01-16 19:36:15 -0300

    Lesson 5: API Calls with Axios

---
+++ src/services/EventService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL:
+    'https://my-json-server.typicode.com/nnnnadia/Real-World-Vue-3-New-Syntax',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+export default {
+  getEvents() {
+    return apiClient.get('/events')
+  },
+}
