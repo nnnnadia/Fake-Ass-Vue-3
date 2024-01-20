c24e09c29d9b94ceca328c80207c413c4e5dac5e
nnnnadia <tristaonadia@gmail.com>
2024-01-20 11:29:55 -0300

    Touring Vue Router - Lesson 9: Wrapping Up

--- src/router/index.js
+++ src/router/index.js
@@ -8,60 +8,69 @@ import About from '@/views/About.vue'
 import NotFound from '@/views/NotFound.vue'
 import NetworkError from '@/views/NetworkError.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    props: (route) => ({ page: +route.query.page || 1 }),
+    component: EventList,
+  },
+  {
+    path: '/event/:id',
+    name: 'EventActions',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails,
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit,
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister,
+      },
+    ],
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true,
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError,
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'EventList',
-      props: (route) => ({ page: +route.query.page || 1 }),
-      component: EventList,
-    },
-    {
-      path: '/event/:id',
-      name: 'EventActions',
-      props: true,
-      component: EventLayout,
-      children: [
-        {
-          path: '',
-          name: 'EventDetails',
-          component: EventDetails,
-        },
-        {
-          path: 'edit',
-          name: 'EventEdit',
-          component: EventEdit,
-        },
-        {
-          path: 'register',
-          name: 'EventRegister',
-          component: EventRegister,
-        },
-      ],
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About,
-    },
-    {
-      path: '/404/:resource',
-      name: '404Resource',
-      component: NotFound,
-      props: true,
-    },
-    {
-      path: '/network-error',
-      name: 'NetworkError',
-      component: NetworkError,
-    },
-    {
-      path: '/:catchAll(.*)',
-      name: 'NotFound',
-      component: NotFound,
-    },
-  ],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
 
 export default router
